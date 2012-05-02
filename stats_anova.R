b19947f96c9faf7b60feacdbf4a1cb1e533a3a86
Mat Gilbert <freefallertam@gmail.com>
2012-05-02 18:23:08 +0100

    Analysis using ezANOVA in R

---
+++ stats_anova.R
@@ -0,0 +1,15 @@
+library(ez)
+
+primary_output_measure_d <- read.csv("~/Dropbox/UniWork/MappingExperiment/D/r_primary_output_measure_d.csv")
+
+attach(primary_output_measure_d)
+
+primary_output_measure_d
+
+dm_anova = ezANOVA(data = primary_output_measure_d,
+        dv = .(completion_time_in_seconds), 
+        wid = .(subject),
+        within = .(world_dimension, screen_dimension),
+        return_aov = TRUE)
+
+print(dm_anova)
